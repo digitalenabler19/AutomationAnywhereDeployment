44addacc90f0c227bf1c7637fc085b5f7368ba99
digitalenabler19 <digitalenabler19@gmail.com>
2020-08-26 18:54:44 +0530

    CR authentication

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+var request = require('request');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -13,7 +14,38 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //console.log(`The event payload: ${payload}`);
+  authenticate('http://ec2-3-6-118-186.ap-south-1.compute.amazonaws.com/','prasad','password1')
 } catch (error) {
   core.setFailed(error.message);
 }
+
+
+function authenticate(CRurl, loginID,password){ 
+var userCred={ Username:loginID, Password:password};
+   request(
+    {headers: {
+      'Content-Type': 'application/json'
+    },
+    url: CRurl+'/v1/authentication',
+    body: JSON.stringify(userCred),
+    method: 'POST'},
+    function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+
+           
+            thisSession = req.session;
+            console.log("results");
+            var parsedBody=JSON.parse(body);
+            console.log(parsedBody);
+    	    	thisSession.token = parsedBody.token;
+            //global.thisSessionToken=parsedBody.token;
+    		console.log('token'+thisSession.token)
+            return thisSession.token;
+        }
+        else  if (body.code=="UM1110" && response.statusCode != 200 )
+        	res.send({status:"Login Failed"});
+    }
+);
+   }
