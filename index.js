bd3fbdbb09250a107a0bc884ecec6d76df2452e4
digitalenabler19 <digitalenabler19@gmail.com>
2020-08-26 18:34:17 +0530

    username

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ try {
   const nameToGreet = core.getInput('who-to-greet');
   const userName = core.getInput('user-name-secret');
   console.log(`Hello Hello1 ${nameToGreet}!`);
-  console.log('testing')
+  console.log('testing');
+  console.log(userName);
   console.log("test"+core.getInput('user-name-secret'));
-  console.log(${userName});
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
